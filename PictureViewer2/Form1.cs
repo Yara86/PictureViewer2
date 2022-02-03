2f0a5fd50c88241a32319e62eca6eecaef39658f
YARA SABRINA ORTIZ GUTIERREZ <201GD654@rcarbonifera.tecnm.mx>
2022-02-02 22:57:41 -0600

    Visor de imágenes para la clase de Tópicos Avanzados de Programación.

--- PictureViewer2/Form1.cs
+++ PictureViewer2/Form1.cs
@@ -16,5 +16,46 @@ namespace PictureViewer2
         {
             InitializeComponent();
         }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Muestra el diálogo de Abrir un archivo.
+            // Si el usuario da click en OK, cargará la imagen que el usuario escogió.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Borra la imagen.
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Muestra el colordialogbox.
+            // Si el usuario da click en OK, cambiará el color de fondo del PictureBox.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Cierra el formulario.
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Si el usuario selecciona el checkbox,
+            // cambiará la propiedad del PictureBox a "Strech".
+            // Si no está seleccionada la propiedad será "Normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
     }
 }
